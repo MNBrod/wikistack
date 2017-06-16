d7528ca3ed6574c7e37dcb51ae3d4b9f9d6fba33
Max Brodheim <max.brodheim@gmail.com>
2017-06-16 15:07:33 -0400

    fixed it kinda?

--- routes/wiki.js
+++ routes/wiki.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 
   router.get('/', function (req, res, next) {
-    // res.send('got to GET /wiki/');
+    res.send('got to GET /wiki/');
     //res.redirect("/wiki");
     console.log("HERE");
   });
